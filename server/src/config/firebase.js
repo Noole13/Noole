b667f565da34466bac3f7371a05b6b3f4ad72f24
Noole13 <hakeerg1212@gmail.com>
2026-09-11 13:01:19 +0300

    Create firebase.js

---
+++ server/src/config/firebase.js
@@ -0,0 +1,23 @@
+const admin = require('firebase-admin');
+require('dotenv').config();
+
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') : undefined,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+};
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET
+  });
+}
+
+const db = admin.database();
+const storage = admin.storage();
+
+module.exports = { admin, db, storage };
